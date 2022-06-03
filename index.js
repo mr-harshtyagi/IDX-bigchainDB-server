1e11a16dfcefb83ae95d146f3b7f10e255af7a1d
mr-harshtyagi <writetoharshtyagi@gmail.com>
2022-06-03 21:13:17 +0530

    array

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const Key = mongoose.model("Key", idxKeySchema);
 const certificateSchema = {
   doc_uid:Number,
   hash: String,
-  certificate_data: Object,
+  certificate_data: Array,
   time_stamp:String
 };
 const Certificate = mongoose.model("Certificate", certificateSchema);
